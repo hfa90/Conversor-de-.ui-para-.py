61e3b407104c62ee8656a7a00ffaeb8ed12f1301
Hayden Fernandes <haydenfernandes.ti@gmail.com>
2024-06-23 21:17:46 -0300

    Update and rename README.md to Convertendo arquivos .ui para .py

---
+++ Convertendo arquivos .ui para .py
@@ -0,0 +1,6 @@
+# Conversor-de-.ui-para-.py
+Conversor de arquivo do Qt Designer (.ui ) para Python (.py)
+
+
+![Captura de tela 2024-06-23 211016](https://github.com/hfa90/Conversor-de-.ui-para-.py/assets/163853542/75469035-bfa1-480d-a67b-e6bc764a8a91)
+![Captura de tela 2024-06-23 211001](https://github.com/hfa90/Conversor-de-.ui-para-.py/assets/163853542/9162e08d-7043-4af9-9972-c1705c9b1080)
--- README.md
+++
@@ -1,2 +0,0 @@
-# Conversor-de-.ui-para-.py
-Conversor de arquivo do Qt Designer (.ui ) para Python (.py)
